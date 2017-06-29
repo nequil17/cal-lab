f507dbac81112a81365136a19e96482a98d72039
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-06-29 16:58:23 -0700

    template

---
+++ main.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+});
+
+var clear = getElementByID('clear');
+clear.addEventListener("click")
+
+btn.appendChild(t);                                
+document.body.appendChild(btn);
+btn.style.border='3px solid blue';
+btn.style.backgroundColor='red';
+
+btn.addEventListener ("click", function() {
+    $('#' + event.which).css('background-color', 'yellow');
+        $(document).keyup(function() {
+            $('#' + event.which).css('background-color', '#f5f5f5');
+        });
+    });
+
+// buttonC.addEventListener('click', function() {
+
+// });
+
+// buttonNine.addEventListener('click', function() {
+
+// });
+
+// buttonEight.addEventListener('click', function() {
+
+// });
+
+// buttonSeven.addEventListener('click', function() {
+
+// });
+
+// buttonSix.addEventListener('click', function() {
+
+// });
+
+// buttonFive.addEventListener('click', function() {
+
+// });
+
+// buttonFour.addEventListener('click', function() {
+
+// });
+
+// buttonThree.addEventListener('click', function() {
+
+// });
+
+// buttonTwo.addEventListener('click', function() {
+
+// });
+
+// buttonOne.addEventListener('click', function() {
+
+// });
